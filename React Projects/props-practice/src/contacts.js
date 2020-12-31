4287be716fc216b396b8a7566a322ab034a34045
Shreyas Vedpathak <shreyasvedpathak@gmail.com>
2020-12-31 17:18:02 +0530

    Used Mapping

--- React Projects/props-practice/src/contacts.js
+++ React Projects/props-practice/src/contacts.js
@@ -1,22 +1,28 @@
 const contacts = [
   {
+    id: 1,
     name: "Beyonce",
-    img: "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    img:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
     contact: "+123 456 789",
-    mail: "b@beyonce.com"
+    mail: "b@beyonce.com",
   },
   {
+    id: 2,
     name: "Jack Bauer",
-    img: "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    img:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
     contact: "+987 654 321",
-    mail: "jack@nowhere.com"
+    mail: "jack@nowhere.com",
   },
   {
+    id: 3,
     name: "Chuck Norris",
-    img: "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    img:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
     contact: "+918 372 574",
-    mail: "gmail@chucknorris.com"
-  }
+    mail: "gmail@chucknorris.com",
+  },
 ];
 
 export default contacts;
