dbd81be628d5df87bf77c868b2ef1ed73effa604
Shreyas Vedpathak <shreyasvedpathak@gmail.com>
2020-12-31 11:24:53 +0530

    Updated Props

--- React Projects/props/src/index.js
+++ React Projects/props/src/index.js
@@ -25,10 +25,10 @@ ReactDOM.render(
       mail="b@beyonce.com"
     />
     <Card
-      name="Beyonce"
-      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      phone="+123 456 789"
-      mail="b@beyonce.com"
+      name="Shreyas Vedpathak"
+      img="https://storage.googleapis.com/kaggle-avatars/images/5433696-kg.jpg"
+      phone="9881898291"
+      mail="babcdefg"
     />
   </div>,
   document.getElementById("root")
